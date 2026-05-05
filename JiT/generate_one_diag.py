51f01efbd2a81185bac25232ceb64d5a12f07b7b
pdyu <12410619@mail.sustech.edu.cn>
2026-05-05 16:40:41 +0800

    full_res

--- JiT/generate_one_diag.py
+++ JiT/generate_one_diag.py
@@ -109,7 +109,7 @@ def main():
     )
     parser.add_argument(
         "--out_dir",
-        default="/home/cvip/deyu/jit_ralu/JiT/result_diag",
+        default="/home/cvip/deyu/jit_ralu/JiT/result_diag_full",
     )
     parser.add_argument("--label", type=int, default=2)
     parser.add_argument("--seed", type=int, default=3)
